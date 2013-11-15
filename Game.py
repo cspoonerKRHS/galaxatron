543c08c0207416e9571611d0f60888b7b24cba2c
Conor McGuire <conor.mcguire@kearsarge.org>
2013-11-15 09:30:47 -0500

    edit things

--- Game.py
+++ Game.py
@@ -1,110 +1,112 @@
-import pygame, sys, math
+import pygame, sys, math, random
 
 pygame.init()
 
-from Ball import Ball
+from Potato import Potato
 from Player import Player
 
-clk = pygame.time.Clock()
+clock = pygame.time.Clock()
 
 width = 640
 height = 480
 size = width, height
 
 screen = pygame.display.set_mode(size)
+
+bgImage = pygame.image.load("rsc/bg/startbg.png")
+bgRect = bgImage.get_rect()
+
 bgColor = r,g,b = 0,0,0
 
-ball1 = Ball(["ball1.png", "ball2.png"], (5,2), (width/2, height/2), (75,75))
-ball2 = Ball(["ball1.png", "ball2.png"], (2,5), (width/3, height/3), (125,125))
-ball3 = Ball(["ball1.png", "ball2.png"], (4,7), (450, 125), (100,100))
-ball4 = Ball(["ball1.png", "ball2.png"], (3,5), (500, 300), (200,200))
+ballp = Player(["rsc/player/playerBall1.png",
+                    "rsc/player/playerBall2.png",
+                    "rsc/player/playerBall3.png"], [3,3], [50,50], [width/2,height/2])
 
-pBall = Player("player.png", (3,3), (550,150), (50,50))
-
+bsize = random.randint(25, 150)
+balls = [Ball("rsc/enemy/ball.png", 
+              [random.randint(-5,5), random.randint(-5,5)], 
+              [bsize, bsize], 
+              [random.randint(75, width-75), random.randint(75, height-75)])]
+              
+start = False
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                pBall.direction("right")
-            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                pBall.direction("left")
-            if event.key == pygame.K_w or event.key == pygame.K_UP:
-                pBall.direction("up")
-            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                pBall.direction("down")
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                pBall.direction("stop right")
-            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                pBall.direction("stop left")
-            if event.key == pygame.K_w or event.key == pygame.K_UP:
-                pBall.direction("stop up")
-            if event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                pBall.direction("stop down")
+    while not start:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    start = True
+        
+        screen.blit(bgImage, bgRect)
+        pygame.display.flip()
+        clock.tick(60)
     
-    ball1.update()
-    ball2.update()
-    ball3.update()
-    ball4.update()
-    pBall.update()
+    bgImage = pygame.image.load("rsc/bg/mainbg.png")
     
+    level = 1
     
-    ball1.wallBounce(width, height)
-    ball2.wallBounce(width, height)
-    ball3.wallBounce(width, height)
-    ball4.wallBounce(width, height)
-    pBall.wallBounce(width, height)
-    
-    
-    ball1.collideBall(ball2)
-    ball1.collideBall(ball3)
-    ball1.collideBall(ball4)
-    ball2.collideBall(ball3)
-    ball2.collideBall(ball4)
-    ball3.collideBall(ball4)
-    
-    pBall.collideBall(ball1)
-    pBall.collideBall(ball2)
-    pBall.collideBall(ball3)
-    pBall.collideBall(ball4)
-    
-    screen.fill(bgColor)
-    screen.blit(ball1.image, ball1.rect)
-    screen.blit(ball2.image, ball2.rect)
-    screen.blit(ball3.image, ball3.rect)
-    screen.blit(ball4.image, ball4.rect)
-    screen.blit(pBall.image, pBall.rect)
-    pygame.display.flip()
-    clk.tick(60)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    while start:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                    ballp.direction("right")
+                if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                    ballp.direction("left")
+                if event.key == pygame.K_w or event.key == pygame.K_UP:
+                    ballp.direction("up")
+                if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                    ballp.direction("down")
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                    ballp.direction("stop right")
+                if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                    ballp.direction("stop left")
+                if event.key == pygame.K_w or event.key == pygame.K_UP:
+                    ballp.direction("stop up")
+                if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                    ballp.direction("stop down")
 
-
-
-
-
-
+        for ball in balls:
+            ball.update()
+        ballp.update()        
+        
+        for ball in balls:
+            ball.collideWall(width, height)
+        ballp.collideWall(width, height)
+        
+        if len(balls) > 1:
+            for first in range(len(balls)-1):
+                for second in range(first+1,len(balls)):
+                    balls[first].collideBall(balls[second])
+        
+        for ball in balls:
+            ballp.collideBall(ball)
+            
+        for ball in balls:
+            if not ball.living:
+                balls.remove(ball)
+                
+        
+        if len(balls) == 0:
+            level += 1
+            for i in range(level):
+                print i
+                bsize = random.randint(25, 150)
+                balls += [Ball("rsc/enemy/ball.png", 
+                              [random.randint(-5,5), random.randint(-5,5)], 
+                              [bsize, bsize], 
+                              [random.randint(75, width-75), random.randint(75, height-75)])]
+                          
+        screen.blit(bgImage, bgRect)
+        screen.blit(ballp.image, ballp.rect)
+        for ball in balls:
+            screen.blit(ball.image, ball.rect)
+        pygame.display.flip()
+        clock.tick(60)
+                
 
 
 
