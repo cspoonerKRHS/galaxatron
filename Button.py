2a259986ff6337283a1d0a5e3acb5b2f507a5778
Chris Spooner <cspooner@kearsarge.org>
2014-02-07 08:40:32 -0500

    broken....sorry

--- Button.py
+++ Button.py
@@ -3,8 +3,7 @@ import pygame, sys, math
 class Button():
     def __init__(self, image, pos = (0,0), size = (100,100)):
         self.image = pygame.image.load(image)
-        self.size = size
-        self.image = pygame.transform.scale(self.image, self.size)
+        self.image = pygame.transform.scale(self.image, size)
         self.rect = self.image.get_rect()
         self.place(pos)
         
@@ -13,10 +12,6 @@ class Button():
             if self.rect.bottom > pt[1] and self.rect.top < pt[1]:
                 return True
         return False
-        
-    def setImage(self, image):
-        self.image = pygame.image.load(image)
-        self.image = pygame.transform.scale(self.image, self.size)
         
     def place(self, pt):
         self.rect.center = pt
